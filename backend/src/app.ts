f103a2fa1d412a8618565d9141b9187bae49c790
Douaa Agr <douinfo11@gmail.com>
2026-09-21 12:55:03 +0100

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://construction-site-knj9.onrender.com/",
+    origin: "https://construction-site-knj9.onrender.com",
     credentials: true,
   }),
 );
